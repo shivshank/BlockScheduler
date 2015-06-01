6f2cd37e056db6274e106baec4268b95546518e9
shivshank <shivshank@users.noreply.github.com>
2015-05-31 21:50:13 -0400

    Fix TableGrid source

--- tablegrid.js
+++ tablegrid.js
@@ -1,19 +1,21 @@
-var TableGrid = {
-	create: function(height, rowWidth1, rowWidth2, rowWidthN) {
+var tableGrid = {
+	create: function(rowWidth1, rowWidth2, rowWidthN) {
 		var args = Array.prototype.slice.call(arguments),
             i, j, t;
 		
-        t = Object.create(TableGrid);
+        t = Object.create(tableGrid);
         t.table = $("<table>");
         t.body = $("<tbody>");
+        t.body.appendTo(t.table);
         
-        for (i=0; i < height; i+=1) {
+        for (i=0; i < args.length; i+=1) {
             row = $("<tr>");
             t.body.append( row );
-            if (args[i+1]) {
-                for (j=0; j < args[i+1]) { row.append( $("<td>") ); }
+            for (j=0; j < args[i]; j+=1) { 
+                row.append( $("<td>") );
             }
         }
+        return t;
 	},
     /** Use the first row (via cloning) for each row  */
     makeUniform: function() {
@@ -41,12 +43,15 @@ var TableGrid = {
         var x, y;
         
         y = 0;
-        this.body.children().forEach( function(i, v) {
+        this.body.children().each( function(i, v) {
             // for each table row
             x = 0;
-            $(v).forEach( function(index, value) {
-                callback(x, y, $(value), $(v));
+            $(v).children().each( function(index, value) {
+                // for each td
+                callback($(value), $(v), x, y);
+                x += 1;
             });
+            y += 1;
         });
     }
 };
